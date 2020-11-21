80b4b50fccd85e0f7353d7d9d9e455fb43064c59
ragnarob <ragnarob@protonmail.com>
2020-11-21 01:55:07 +0100

    Implement password forget/reset

--- app/api/auth-router.js
+++ app/api/auth-router.js
@@ -1,6 +1,8 @@
 import BaseRouter from './baseRouter.js'
 import { sendEmail } from '../emailFacade.js'
 import bcrypt from 'bcrypt'
+import dateFns from 'date-fns'
+const { isAfter, addHours } = dateFns
 const { compare, hash } = bcrypt
 
 export default class AuthenticationRouter extends BaseRouter {
@@ -17,6 +19,8 @@ export default class AuthenticationRouter extends BaseRouter {
     this.app.post('/changepassword', this.authorizeUser.bind(this), (req, res) => this.changePassword(req, res))
     this.app.post('/changeusername', this.authorizeUser.bind(this), (req, res) => this.changeUsername(req, res))
     this.app.post('/change-email', this.authorizeUser.bind(this), (req, res) => this.changeEmail(req, res))
+    this.app.post('/reset-password', (req, res) => this.resetPassword(req, res))
+    this.app.post('/reset-password-link/:token', (req, res) => this.resetPasswordByLink(req, res))
   }
 
   async refreshAuth (req, res) {
@@ -214,6 +218,79 @@ export default class AuthenticationRouter extends BaseRouter {
     }
   }
 
+  async resetPassword (req, res) {
+    try {
+      let a={}
+      a.asd.asd()
+      let email = req.body.email
+      if (!this.validateEmail(email)) {
+        return this.returnStatusError(400, res, 'This is not a valid email address')
+      }
+
+      let user = await this.getUserByEmail(email)
+      if (user) {
+        let resetToken = generateRandomString(30)
+        let insertQuery = 'INSERT INTO resettoken (Token, UserId) VALUES (?, ?)'
+        let queryParams = [resetToken, user.id]
+        await this.databaseFacade.execute(insertQuery, queryParams)
+
+        await sendEmail(
+          'account',
+          email,
+          'Password reset - Yiffer.xyz',
+          `You have requested a password reset for your account. Click the link below to create a new password. The link will expire in 24 hours.
+          <br/><br/>
+          <strong>https://beta.yiffer.xyz/reset-password-link/${resetToken}</strong>
+          <br/><br/>Regards, Yiffer.xyz`
+        )
+      }
+
+      res.status(204).end()
+    }
+    catch (err) { return this.returnStatusError(500, res, err) }
+  }
+
+  async resetPasswordByLink (req, res) {
+    try {
+      let token = req.params.token
+      let [password1, password2] = [req.body.password1, req.body.password2]
+      if (password1 !== password2) {
+        return this.returnStatusError(400, res, 'Passwords do not match')
+      }
+      if (!this.validatePassword(password1)) {
+        return this.returnStatusError(400, res, 'Invalid password')
+      }
+
+      let tokenQuery = 'SELECT UserId AS userId, Token AS token, Timestamp AS timestamp, IsUsed AS isUsed FROM resettoken WHERE token = ?'
+      let tokenResults = await this.databaseFacade.execute(tokenQuery, [token])
+      if (tokenResults.length === 0) {
+        return this.returnStatusError(404, res, 'Invalid link')
+      }
+      let resetRecord = tokenResults[0]
+
+      if (resetRecord.isUsed) {
+        return this.returnStatusError(400, res, 'This link has been used already.')
+      }
+
+      let maxTokenUsageTime = addHours(new Date(resetRecord.timestamp), 24)
+      if (isAfter(new Date(), maxTokenUsageTime)) {
+        return this.returnStatusError(400, res, 'Link expired. Submit a password reset request again.')
+      }
+
+      let hashedPassword = await hash(password1, 8)
+
+      let updateQuery = 'UPDATE user SET Password = ? WHERE Id = ?'
+      let updateQueryParams = [hashedPassword, resetRecord.userId]
+      await this.databaseFacade.execute(updateQuery, updateQueryParams, 'Error updating password in database')
+
+      let updateResetTokenQuery = 'UPDATE resettoken SET IsUsed=1 WHERE Token=?'
+      await this.databaseFacade.execute(updateResetTokenQuery, [token], 'Error updating reset token database')
+
+      res.status(204).end()
+    }
+    catch (err) { return this.returnStatusError(500, res, err) }
+  }
+
   validatePassword (password) {
     return password.length >= 6
   }
@@ -224,4 +301,28 @@ export default class AuthenticationRouter extends BaseRouter {
     // eslint-disable-next-line no-control-regex
     return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
   }
+
+  async getUserByEmail (email) {
+    let query = `
+      SELECT Id AS id, Username AS username, Email AS email, UserType AS userType, CreatedTime AS createdTime
+      FROM user WHERE email = ?
+    `
+    let results = await this.databaseFacade.execute(query, [email], 'Error looking up user in database')
+    if (!results || results.length === 0) {
+      return null
+    }
+    else {
+      return results[0]
+    }
+  }
+}
+
+function generateRandomString (length) {
+  var result           = ''
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  var charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
 }
--- app/emailFacade.js
+++ app/emailFacade.js
@@ -4,11 +4,16 @@ import postmark from 'postmark'
 let client = new postmark.ServerClient(emailSettings.apiKey);
 
 export async function sendEmail (sendername, receiver, subject, text) {
-  client.sendEmail({
-    From: `${sendername}@yiffer.xyz`,
-    To: 'contact@yiffer.xyz',
-    Subject: subject,
-    HtmlBody: text,
-    MessageStream: 'outbound'
-  });
+  try {
+    await client.sendEmail({
+      From: `${sendername}@yiffer.xyz`,
+      To: 'contact@yiffer.xyz',
+      Subject: subject,
+      HtmlBody: text,
+      MessageStream: 'outbound'
+    });
+  }
+  catch (err) {
+    throw {error: err, message: 'Error sending email'}
+  }
 }
